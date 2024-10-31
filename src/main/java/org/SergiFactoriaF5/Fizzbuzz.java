c2f3daef55a243a290b10c2172c53810515cc56b
Sergi <sergi.prof@hotmail.com>
2024-10-31 15:20:55 +0100

    feat: add FizzBuzz test

--- src/main/java/org/SergiFactoriaF5/Fizzbuzz.java
+++ src/main/java/org/SergiFactoriaF5/Fizzbuzz.java
@@ -2,14 +2,14 @@ package org.SergiFactoriaF5;
 
 public class Fizzbuzz {
     public String checkNumber( int number){
+        if(number % 3 == 0 && number % 5 == 0){
+            return"FizzBuzz";
+        }
         if(number % 3 == 0){
             return "Fizz";
         }
         if(number % 5 == 0){
             return "Buzz";
-        }
-        if(number % 3 == 0 && number % 5 == 0){
-            return"FizzBuzz";
         }
         return null;
     }
